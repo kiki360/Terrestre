f789e8e1b2e231071e9b7ef42d508194e025f908
Calliope Epstein-Pawlak <kiki360@rootlike.com>
2025-04-17 12:32:50 -0500

    I added a file named SettingsPage with a similarly named view

---
+++ SettingsPage.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsPage.swift
+//  ClimateChangeGroupApp
+//
+//  Created by Calliope Epstein-Pawlak on 4/17/25.
+//
+
+import SwiftUI
+
+struct SettingsPage: View {
+    var body: some View {
+        VStack {
+            Text("Credits:")
+                .font(.largeTitle)
+                .bold()
+        }
+    }
+}
+
+#Preview {
+    SettingsPage()
+}
