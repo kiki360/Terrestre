a8a6c63c9c6775a6c52e8f5b93286895dbe2c24d
Calliope Epstein-Pawlak <kiki360@rootlike.com>
2025-04-17 13:09:57 -0500

    I added a file named GamePhysics.

---
+++ GamePhysics.swift
@@ -0,0 +1,13 @@
+//
+//  GamePhysics.swift
+//  ClimateChangeGroupApp
+//
+//  Created by Calliope Epstein-Pawlak on 4/17/25.
+//
+
+// I learned most of this code from ChatGPT, not the Sprites/Physics Learning in Schoology. The notes I took while using ChatGPT are in a different XCode project and I copied most of it for this file
+
+import SwiftUI
+import SpriteKit
+
+
