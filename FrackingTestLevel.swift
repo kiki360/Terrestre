da075f11f579360b303cd4a41ef3e2f67be97f64
Jacob <jacobcaulfield10@gmail.com>
2025-04-17 08:14:23 -0500

    I added a link to cite the article I used to find information

--- FrackingTestLevel.swift
+++ FrackingTestLevel.swift
@@ -8,6 +8,6 @@ import SwiftUI
 
 struct FrackingView: View {
     var body: some View {
-        Text("")
+        Link("How to Tackle Fracking in Your Community", destination: (URL(string: "https://www.nrdc.org/stories/how-tackle-fracking-your-community")) ?? URL(string: "https://www.apple.com")!)
     }
 }
