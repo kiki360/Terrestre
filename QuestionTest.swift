b7eac6128b7858ad637906c91d101c5bea9c302e
Jacob <jacobcaulfield10@gmail.com>
2025-04-23 14:56:28 -0500

    I switched the textfields to try and use a variable specific to them. Also i don't know what the Breakpoints_v2.xcbkptlist is or why it thinks I made an edit to it.

--- QuestionTest.swift
+++ QuestionTest.swift
@@ -15,15 +15,14 @@ struct Question: Identifiable {
 struct QuestionTest: View {
     @State var questionAnswers: [UUID: String] = [:]
     let questions: [Question] = [Question(text: "Fracking is bad for the environment.", answers: [true: "Correct. Fracking releases a lot of carbon dioxide into the atmosphere, and causes chaos in neighborhoods.", false: "Incorrect. Fracking is bad for the environment."]), Question(text: "The answer for this question is true", answers: [true: "correct", false: "incorrect"])]
-    @State var answerTemporary: String = ""
     @State var answer: String = ""
     var body: some View {
         List{
             ForEach(questions, id: \Question.id){ question in
                 Section(){
-//                    questionAnswers
+                    @State var answerTemporary: String = ""
                     Text(question.text)
-//                    TextField("Enter your answer as true or false", text: $questionAnswers[question.id])
+                    TextField("Enter your answer as true or false", text: $answerTemporary)
                         .onSubmit {
                             questionAnswers[question.id] = answerTemporary
                             answerTemporary = ""
