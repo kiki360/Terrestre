64d883bc99e149b76e1fe692c006cd201b3e9b60
Bibi <bgonzalez7140@stu.d214.org>
2025-04-24 12:54:33 -0500

    testing out format and text for the statistics page (added textfield and hstack)

--- StatisticsPage.swift
+++ StatisticsPage.swift
@@ -11,6 +11,11 @@ struct StatisticsPage: View {
     var body: some View {
         Text("Statistics About Climate Change")
             .font(.custom("Courier", size: 40))
+        HStack{
+            Text("''The bottled water industry generated roughly 600 billion plastic bottles and containers in 2021, according to the report. This resulted in around 25 million tons of plastic waste — most of which is not recycled and ends up in landfills.''")
+                .font(.custom("Courier", size: 20))
+            //BIBI!!! add a button that redirects to the articles. i lowkey forgot how to do it tho so i gotta remind myself rq.
+        }
     }
 }
 
