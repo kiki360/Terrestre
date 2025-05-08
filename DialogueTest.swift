7617824bb0ffacab50685d686af87795015ca2f1
Arcbuddy <jacobcaulfield10@gmail.com>
2025-05-08 13:37:59 -0500

    I'm trying to make it so the user can respond

--- DialogueTest.swift
+++ DialogueTest.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct DialogueTest: View {
     @AppStorage("username") var username = "Guest"
+//    @State var Responses: [Int: [String]] = [1: ["Yes, I will help.", "No, I will not help."]]
     let TotalIterations: Int = 2
     @State var iterations: Int = 1
     @State var showHistory: Bool = false
+    @State var History: [String] = []
     var body: some View {
         //        Text("\(Dialogue())")
         if showHistory{
@@ -26,6 +28,14 @@ struct DialogueTest: View {
                     iterations = TotalIterations
                 }
             }
+//            ForEach(0...iterations, id: \.self){ text in
+//                if let Display = Responses[text]{
+//                    Text("\(Display)")
+//                } else {
+//                    Text("Error")
+//                }
+//                
+//            }
         }
         Button("\(ButtonName())"){
             showHistory.toggle()
@@ -43,8 +53,8 @@ struct DialogueTest: View {
     //        }
     //    }
     func Dialogue(Iteration: Int) -> String{
-        let Dialogue: [Int: String] = [1: "Hi, \(username)! We need your help to stop the fracking in our town. To do this, could you please contact our local Town Hall?",2: "Thanks! We will surely remember this when you return."]
-        if let iteration = Dialogue[Iteration]{
+        let dialogue = [1: "Hi, \(username)! We need your help to stop the fracking in our town. To do this, could you please contact our local Town Hall?",2: "Thanks! We will surely remember this when you return."]
+        if let iteration = dialogue[Iteration]{
             return iteration
         }
         return ""
@@ -57,5 +67,5 @@ struct DialogueTest: View {
         }
     }
 }
-// dialogue: ["Hi, \(username)! We need your help to stop the fracking in our town. To do this, could you please contact our local Town Hall?", "Thanks! We will surely remember this when you return."], totalIteration: 2)
 
+//var dialogue: [Int: String] = [1: "Hi, \(username)! We need your help to stop the fracking in our town. To do this, could you please contact our local Town Hall?",2: "Thanks! We will surely remember this when you return."]
