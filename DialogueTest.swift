4e4574631692aa252dd44cf523309ac4fed9210e
Arcbuddy <jacobcaulfield10@gmail.com>
2025-05-12 13:11:46 -0500

    This should've fixed the error i mentioned before

--- DialogueTest.swift
+++ DialogueTest.swift
@@ -7,121 +7,55 @@
 
 import SwiftUI
 
-//struct DialogueTest: View {
-//    @AppStorage("username") var username = "Guest"
-//    let TotalIterations: Int = 2
-//    @State var iterations: Int = 1
-//    @State var showHistory: Bool = false
-//    @State var Response: Bool = false
-//    let ResponseOptions: [Int: [Int: String]] = [1: [1: "Yes, I will help.",2: "No, I will not help."]]
-//    @State var numberOfButtons: Int = 0
-//    //Change Response to a let constant later
-//    //let Response: Bool
-//    var body: some View {
-//        Button("Response? \(Response)"){
-//            Reset()
-//            Response.toggle()
-//        }
-//        if Response {
-//            if showHistory{
-//                ForEach(0...iterations, id: \.self){ text in
-//                    Text("\(DialogueNoResponse(Iteration: text))")
-//                }
-//            } else {
-//                //MARK: Response Version Here
-//                Text("\(DialogueResponse(purpose: "Text"))")
-//                HStack{
-//                    ForEach(ResponseOptions[1]?.count, id: \.self){ text in
-//                        Button("\(ResponseOptions[text] ?? [:][text] ?? "Error")") {
-//                            ButtonClicked(WhichOne: text)
-//                        }
-//                    }
-//                }
-//                }
-//        } else {
-//            //        Text("\(Dialogue())")
-//            if showHistory{
-//                ForEach(0...iterations, id: \.self){ text in
-//                    Text("\(DialogueNoResponse(Iteration: text))")
-//                }
-//            } else {
-//                Text("\(DialogueNoResponse(Iteration: iterations))")
-//                Button("Next"){
-//                    iterations += 1
-//                    if iterations > TotalIterations{
-//                        iterations = TotalIterations
-//                    }
-//                }
-//                //            ForEach(0...iterations, id: \.self){ text in
-//                //                if let Display = Responses[text]{
-//                //                    Text("\(Display)")
-//                //                } else {
-//                //                    Text("Error")
-//                //                }
-//                //
-//                //            }
-//            }
-//            Button("\(ButtonName())"){
-//                showHistory.toggle()
-//            }
+struct DialogueTest: View {
+    @AppStorage("username") var username = "Guest"
+    let TotalIterations: Int = 2
+    @State var iterations: Int = 1
+    @State var showHistory: Bool = false
+    @State var Response: Bool = false
+    //Change Response to a let constant later
+    //let Response: Bool
+    var body: some View {
+        Button("Response? \(Response)"){
+            Reset()
+            Response.toggle()
+        }
+        if Response {
+            //Do not go into this it will crash the app
+//            DialogueTestResponse(iterations: $iterations, totalIterations: TotalIterations)
+            //It should work once Mr. Brown updates the macbooks to the newest MacOS version
+        } else {
+            DialogueTestNoResponse(TotalIterations: TotalIterations, iterations: $iterations, showHistory: $showHistory)
+        }
+    }
+//    func Dialogue() -> String{
+//        let Dialogue: [Int: String] = [1: "Hi, \(username)! We need your help to stop the fracking in our town. To do this, could you please contact our local Town Hall?",2: "Thanks! We will surely remember this when you return."]
+//        switch iterations{
+//        case 1:
+//            return Dialogue[1]!
+//        case 2:
+//            return Dialogue[1]! + "\n" + Dialogue[2]!
+//        default:
+//            return ""
 //        }
 //    }
-//    //    func Dialogue() -> String{
-//    //        let Dialogue: [Int: String] = [1: "Hi, \(username)! We need your help to stop the fracking in our town. To do this, could you please contact our local Town Hall?",2: "Thanks! We will surely remember this when you return."]
-//    //        switch iterations{
-//    //        case 1:
-//    //            return Dialogue[1]!
-//    //        case 2:
-//    //            return Dialogue[1]! + "\n" + Dialogue[2]!
-//    //        default:
-//    //            return ""
-//    //        }
-//    //    }
-//    func DialogueNoResponse(Iteration: Int) -> String{
-//        let dialogue = [1: "Hi, \(username)! We need your help to stop the fracking in our town. To do this, could you please contact our local Town Hall?", 2: "Thanks! We will surely remember this when you return."]
-//        if let iteration = dialogue[Iteration]{
-//            return iteration
-//        }
-//        return ""
-//    }
-//    func Reset(){
-//        iterations = 1
-//        showHistory = false
-//        numberOfButtons = 0
-//    }
-//    func DialogueResponse(purpose: String) -> String{
-//        let dialogue = [1: "Hi, \(username)! We need your help to stop the fracking in our town. To do this, could you please contact our local Town Hall?",2: "Thanks! We will surely remember this when you return."]
-//        if purpose == "Button1Name"{
-//            if let name = ResponseOptions[1]{
-//                return name[1] ?? ""
-//            }
-//        } else if purpose == "Button2Name"{
-//            if let name = ResponseOptions[1]{
-//                return name[2] ?? ""
-//            }
-//        } else if purpose == "Text"{
-//            return dialogue[iterations] ?? ""
-//        }
-//        return ""
-//    }
-//    func NumberOfButtons(){
-//        if Int(DialogueResponse(purpose: "NumberOfButtons")) ?? 0 == 2{
-//            
-//        }
-//    }
-//    func ButtonClicked(WhichOne: Int){
-//        
-//    }
-//    func ButtonName() -> String{
-//        if showHistory{
-//            return "Hide Chat History"
-//        } else {
-//            return "Show Chat History"
-//        }
-//    }
-//}
-//
-////var dialogue: [Int: String] = [1: "Hi, \(username)! We need your help to stop the fracking in our town. To do this, could you please contact our local Town Hall?",2: "Thanks! We will surely remember this when you return."]
-//#Preview {
-//    DialogueTest()
-//}
+func Reset(){
+    iterations = 1
+    showHistory = false
+}
+func ButtonClicked(WhichOne: Int){
+    
+}
+    func ButtonName() -> String{
+        if showHistory{
+            return "Hide Chat History"
+        } else {
+            return "Show Chat History"
+        }
+    }
+}
+
+//var dialogue: [Int: String] = [1: "Hi, \(username)! We need your help to stop the fracking in our town. To do this, could you please contact our local Town Hall?",2: "Thanks! We will surely remember this when you return."]
+#Preview {
+    DialogueTest()
+}
